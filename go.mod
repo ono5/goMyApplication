11e0ee1a30444edb9af03f6fa51f17a6fa4a1270
ono5 <kuroneko_head_lord@yahoo.co.jp>
2023-10-30 06:20:38 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ono5/myGoWebApplication
+
+go 1.21.1
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+const portNumber = ":8080"
+
+// Home is the handler for the home page
+func Home(w http.ResponseWriter, r *http.Request) {
+}
+
+// About is the handler for the about page
+func About(w http.ResponseWriter, r *http.Request) {
+}
+
+func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
+}
