0d2991580bd6b8697242ba40c667c6989935b53b
ono5 <kuroneko_head_lord@yahoo.co.jp>
2023-11-01 06:35:26 +0900

    add base template

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -7,7 +7,7 @@ import (
 )
 
 func RenderTemplate(w http.ResponseWriter, tpml string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tpml)
+	parsedTemplate, _ := template.ParseFiles("./templates/"+tpml, "./templates/base-layout.tpml")
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("error parsing template:", err)
